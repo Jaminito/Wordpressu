a7d7ad88939fe6b98243670385f7069c75386bbe
Santi <santialcantaramartin@gmail.com>
2020-07-10 17:03:58 +0200

    estilo dado

--- Formulariacion.php
+++ Formulariacion.php
@@ -6,11 +6,46 @@
  * Author: Jamini
  * Author URI: http://alturl.com/p749b
  */
+// Cuando el plugin se active se crea la tabla para recoger los datos si no existe
+register_activation_hook(__FILE__, 'Kfp_Aspirante_init');
+ 
+/**
+ * Crea la tabla para recoger los datos del formulario
+ *
+ * @return void
+ */
+function formulariacion_Aspirante_init() 
+{
+    global $wpdb; // Este objeto global permite acceder a la base de datos de WP
+    // Crea la tabla sólo si no existe
+    // Utiliza el mismo prefijo del resto de tablas
+    $tabla_aspirantes = $wpdb->prefix . 'aspirante';
+    // Utiliza el mismo tipo de orden de la base de datos
+    $charset_collate = $wpdb->get_charset_collate();
+    // Prepara la consulta
+    $query = "CREATE TABLE IF NOT EXISTS $tabla_aspirantes (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        nombre varchar(40) NOT NULL,
+        correo varchar(100) NOT NULL,
+        nivel_html smallint(4) NOT NULL,
+        nivel_css smallint(4) NOT NULL,
+        nivel_js smallint(4) NOT NULL,
+        aceptacion smallint(4) NOT NULL,
+        created_at datetime NOT NULL,
+        UNIQUE (id)
+        ) $charset_collate;";
+    // La función dbDelta permite crear tablas de manera segura se
+    // define en el archivo upgrade.php que se incluye a continuación
+    include_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta($query); // Lanza la consulta para crear la tabla de manera segura
+}
+
 add_shortcode('formulariacion_aspirante','formulariacion_Aspirante');
 
 function formulariacion_Aspirante() 
 {
     // Esta función de PHP activa el almacenamiento en búfer de salida (output buffer)
+    wp_enqueue_style('css_aspirante', plugins_url('style.css', __FILE__));
     // Cuando termine el formulario lo imprime con la función ob_get_clean
     ob_start();
     ?>
