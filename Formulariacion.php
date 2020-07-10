335abda9d56df21df1378017136f05b96db069a5
Santi <santialcantaramartin@gmail.com>
2020-07-10 17:12:14 +0200

    CSRF security

--- Formulariacion.php
+++ Formulariacion.php
@@ -53,6 +53,8 @@ function formulariacion_Aspirante()
         AND $_POST['nivel_css'] != ''
         AND $_POST['nivel_js'] != ''      
         AND $_POST['aceptacion'] == '1'
+        AND wp_verify_nonce($_POST['aspirante_nonce'], 'graba_aspirante')
+
     ) {
         $tabla_aspirantes = $wpdb->prefix . 'aspirante'; 
         $nombre = sanitize_text_field($_POST['nombre']);
@@ -84,6 +86,7 @@ function formulariacion_Aspirante()
     ?>
     <form action="<?php get_the_permalink(); ?>" method="post" id="form_aspirante"
 class="cuestionario">
+<?php wp_nonce_field('graba_aspirante', 'aspirante_nonce'); ?>
         <div class="form-input">
             <label for="nombre">Nombre</label>
             <input type="text" name="nombre" id="nombre" required>
